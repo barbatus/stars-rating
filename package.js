26aa7277f19974868e7cb4343a684ea5d6088a8d
barbatus <alex.borodach@gmail.com>
2015-05-24 16:20:33 +0300

    Removes font Awesome package

--- package.js
+++ package.js
@@ -9,7 +9,6 @@ Package.describe({
 Package.onUse(function(api) {
     api.versionsFrom('1.0.3.2');
     api.use(['less', 'underscore', 'jquery', 'templating'], 'client');
-    api.use(['fortawesome:fontawesome@4.2.0_2'], 'client');
     api.addFiles([
         'stars_rating.html',
         'stars_rating.js',
