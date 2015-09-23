6358644da89268f864496be4dff8860f6d58a851
barbatus <alex.borodach@gmail.com>
2015-09-23 22:45:40 +0300

    Changed to 1.2v.

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
     name: 'barbatus:stars-rating',
-    version: '1.0.3',
+    version: '1.0.4',
     summary: 'Stars rating control',
     git: 'https://github.com/barbatus/stars-rating',
     documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.0.3.2');
-    api.use(['less@2.5.0_2', 'underscore', 'jquery', 'templating'], 'client');
+    api.versionsFrom('1.2');
+    api.use(['less', 'underscore', 'jquery', 'templating'], 'client');
     api.addFiles([
         'stars_rating.html',
         'stars_rating.js',
