3bbbaf987b878e6de64b711a665caf0ef9c4b15a
Sarah Mattar <sarah.mattar@mac.com>
2020-09-20 14:49:52 -0700

    Updated minimum requirements for less, jquery, and underscore to match Meteor 1.9+'s requirements of jquery@3.0.0 and newer.

--- package.js
+++ package.js
@@ -1,26 +1,31 @@
 Package.describe({
-  name: 'barbatus:stars-rating',
-  version: '1.1.0',
-  summary: 'Stars rating control',
-  git: 'https://github.com/barbatus/stars-rating',
-  documentation: 'README.md'
+	name: 'barbatus:stars-rating',
+	version: '1.1.0',
+	summary: 'Stars rating control',
+	git: 'https://github.com/barbatus/stars-rating',
+	documentation: 'README.md',
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom(['1.0.3.2', '1.2']);
-  api.use([
-    'less', 'underscore', 'jquery',
-    'templating', 'ecmascript'
-  ], 'client');
-  api.addFiles([
-    'stars_rating.html',
-    'stars_rating.js',
-    'stars_rating.less'
-  ], 'client');
+Package.onUse(function (api) {
+	api.versionsFrom('1.9');
+	api.use(
+		[
+			'less@2.8.0',
+			'underscore@1.0.10',
+			'jquery@3.0.0',
+			'templating',
+			'ecmascript',
+		],
+		'client'
+	);
+	api.addFiles(
+		['stars_rating.html', 'stars_rating.js', 'stars_rating.less'],
+		'client'
+	);
 });
 
-Package.onTest(function(api) {
-    api.use('tinytest');
-    api.use('barbatus:stars-rating');
-    api.addFiles('stars_rating-tests.js');
+Package.onTest(function (api) {
+	api.use('tinytest');
+	api.use('barbatus:stars-rating');
+	api.addFiles('stars_rating-tests.js');
 });
